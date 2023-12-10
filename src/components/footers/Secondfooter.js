22ecebf6bf06dd85146786c05c5a9d7c7a77dd0a
outman1998 <outmanmounir98@gmail.com>
2023-12-10 20:04:35 +0100

    modal added and second nav added

--- src/components/footers/Secondfooter.js
+++ src/components/footers/Secondfooter.js
@@ -6,10 +6,10 @@ export default function Secondfooter() {
     const {isOpen, onOpen, onOpenChange} = useDisclosure();
 
   return (
-    <div className='fixed bottom-1 left-1/2 bg-opacity-80 backdrop-blur-md transform -translate-x-1/2 transform -translate-y-1/2 w-[80%] px-4 py-2 lg:w-fit justify-center rounded-md'>
+    <div className='fixed bottom-1 left-1/2 bg-opacity-80 backdrop-blur-md transform -translate-x-1/2 transform -translate-y-1/2 px-4 py-2 justify-center rounded-md w-[350px]'>
 
-        <ul className='flex justify-center rounded-md items-center'>
-            <li className='mr-8 font-bold text-[#fcda9a]'>
+        <ul className='flex justify-between rounded-md items-center'>
+            <li className=' font-bold text-[#fcda9a]'>
                 <Button 
                 onPress={onOpen}
                 className='m-0 p-0 bg-transparent text-[#fcda9a] font-bold text-md'
@@ -38,7 +38,7 @@ export default function Secondfooter() {
                     </ModalContent>
                 </Modal>
             </li>
-            <li className='mr-8 font-bold text-[#fcda9a]'>Smiley Rapport</li>
+            <li className='font-bold text-[#fcda9a]'>Smiley Rapport</li>
             <li className='font-bold text-[#fcda9a]'>Third Item</li>
         </ul>
 
