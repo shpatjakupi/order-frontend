50baa531d1b89c3228929f02552cfe8066ecb5e9
outman1998 <outmanmounir98@gmail.com>
2024-02-06 16:00:55 +0100

    admin / active orders component

--- src/components/admin/AktiveOrdrer.js
+++ src/components/admin/AktiveOrdrer.js
@@ -1,136 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { FaPhone } from "react-icons/fa";
 import {Textarea} from "@nextui-org/react";
 import { MdDeliveryDining } from "react-icons/md";
 
 const AktiveOrdrer = () => {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://order.eu-north-1.elasticbeanstalk.com/admin/getTodaysOrders', {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Basic ' + btoa('admin:admin'), // assuming basic auth, adjust as needed
+            'Content-Type': 'application/json',
+            // Add any other headers if required
+        },
+        });
+        
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        setOrders(data);
+        console.log(orders);
+      } catch (error) {
+        console.error('Error fetching data:', error.message);
+      }
+    };
+
+    fetchData();
+  }, []); 
+
   return (
-    <div className='mt-5'>
-
-        <div className='flex gap-x-5 mx-2'>
+    <div className='flex gap-x-5 mx-2'>
+        {orders.map((orderData, index) => (
+        <>
+          <div className='bg-blue-500 p-2' key={index}>
+            <h2>Name: {orderData.order.name}</h2>
+            <p>Details: {orderData.order.details}</p>
+            <p>Price: {orderData.order.fullPrice}</p>
+            <p>Pickup Date: {orderData.order.pickUpDate}</p>
+            <p>Comment: {orderData.order.comment}</p>
             
-            <div className='bg-gray-100 p-4 rounded-md'>
-    
-                <div className='flex justify-between items-center border-b border-gray-100 pb-3'>
-                    <div className='flex items-center gap-x-2'>
-                        Levering 
-                        <MdDeliveryDining className='text-2xl' />
-                    </div>
-                    <p className='border-l border-gray-400 pl-2 ml-2'>#313</p>
-                    <div className='flex items-center gap-x-2 border-r border-l border-gray-400 px-3 mx-3'>
-                        <p>Josefine K.</p> 
-                        <FaPhone />
-                    </div>
-                    <p className='bg-red-400 text-white rounded-md p-1'>20 min.</p>
-                </div>
-
-                <div className='my-3'>
-                <Textarea
-                    label="Kommentar"
-                    labelPlacement="outside"
-                    className='w-full rounded-md text-sm'
-                    isReadOnly
-                    placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco'
-                />
-                </div>
-
-                <div className='flex gap-x-2 bg-white my-3 p-2 rounded-md'>
-                    <p className='rounded-full p-1 px-[11px] bg-green-600 text-white w-fit h-fit mr-5 text-sm'>1</p>
-                    <div>
-                    <p className='font-bold border-b mb-2 pb-1'>Burger menu</p>
-                    <p className='text-sm'> <span className='font-bold'>Burger:</span> cheese okse</p>
-                    <p className='text-sm'> <span className='font-bold'>Dip:</span> Salatmayonaise</p>
-                    <p className='text-sm'> <span className='font-bold'>Sodavand:</span> Cola</p>
-
-                    </div>
-                </div>
-
-                <div className='flex gap-x-2 bg-white my-3 p-2 rounded-md'>
-                    <p className='rounded-full p-1 px-[11px] bg-green-600 text-white w-fit h-fit mr-5 text-sm'>1</p>
-                    <div>
-                    <p className='font-bold border-b mb-2 pb-1'>Pizza Blanca</p>
-                    <p className='text-sm'> <span className='font-bold'>Topping:</span> Parmesan</p>
-
-                    </div>
-                </div>
-
-                <div>
-                    Pris: 420 kr.
-                </div>
-
-                <div className='mt-3 flex justify-between items-center gap-x-4'>
-                    <button
-                    className='bg-green-600 text-white py-2 rounded-md font-bold w-full'
-                    >
-                        Klar til levering
-                    </button>
-                    <button className='bg-gray-300 p-2 rounded-md font-bold'>...</button>
-                </div>
-            </div>
-
-            <div className='bg-gray-100 p-4 rounded-md'>
-    
-                <div className='flex justify-between items-center border-b border-gray-100 pb-3'>
-                    <div className='flex items-center gap-x-2'>
-                        Levering 
-                        <MdDeliveryDining className='text-2xl' />
-                    </div>
-                    <p className='border-l border-gray-400 pl-2 ml-2'>#313</p>
-                    <div className='flex items-center gap-x-2 border-r border-l border-gray-400 px-3 mx-3'>
-                        <p>Josefine K.</p> 
-                        <FaPhone />
-                    </div>
-                    <p className='bg-red-400 text-white rounded-md p-1'>20 min.</p>
-                </div>
-
-                <div className='my-3'>
-                <Textarea
-                    label="Kommentar"
-                    labelPlacement="outside"
-                    className='w-full rounded-md text-sm'
-                    isReadOnly
-                    placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco'
-                />
-                </div>
-
-                <div className='flex gap-x-2 bg-white my-3 p-2 rounded-md'>
-                    <p className='rounded-full p-1 px-[11px] bg-green-600 text-white w-fit h-fit mr-5 text-sm'>1</p>
-                    <div>
-                    <p className='font-bold border-b mb-2 pb-1'>Burger menu</p>
-                    <p className='text-sm'> <span className='font-bold'>Burger:</span> cheese okse</p>
-                    <p className='text-sm'> <span className='font-bold'>Dip:</span> Salatmayonaise</p>
-                    <p className='text-sm'> <span className='font-bold'>Sodavand:</span> Cola</p>
-
-                    </div>
-                </div>
-
-                <div className='flex gap-x-2 bg-white my-3 p-2 rounded-md'>
-                    <p className='rounded-full p-1 px-[11px] bg-green-600 text-white w-fit h-fit mr-5 text-sm'>1</p>
-                    <div>
-                    <p className='font-bold border-b mb-2 pb-1'>Pizza Blanca</p>
-                    <p className='text-sm'> <span className='font-bold'>Topping:</span> Parmesan</p>
-
-                    </div>
-                </div>
-
-                <div>
-                    Pris: 420 kr.
-                </div>
-
-                <div className='mt-3 flex justify-between items-center gap-x-4'>
-                    <button
-                    className='bg-green-600 text-white py-2 rounded-md font-bold w-full'
-                    >
-                        Klar til levering
-                    </button>
-                    <button className='bg-gray-300 p-2 rounded-md font-bold'>...</button>
-                </div>
-            </div>
-
-        </div>
-
+            <h3 className='mt-3'>Items:</h3>
+            <ul>
+              {orderData.order.items.map(item => (
+                <li key={item.description}>
+                  <p>Description: {item.description}</p>
+                  <p>Price: {item.price}</p>
+                  <p>Quantity: {item.quantity}</p>
+                  {/* Add more details as needed */}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default AktiveOrdrer
+export default AktiveOrdrer;
